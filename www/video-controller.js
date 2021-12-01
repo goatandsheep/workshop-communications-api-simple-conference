4f0af53f8ef5cf17d37e218b2f8ead303ad8ed5d
Jessa Mae Alcantara <the.epikskeptic@gmail.com>
2021-11-30 20:49:56 -0500

    Added libraries

--- www/video-controller.js
+++ www/video-controller.js
@@ -153,17 +153,6 @@
    let videoNode = document.getElementById('video-clip');
 
    if (!videoNode) {
-    let track = document.createElement("track");
-    track.kind = "captions";
-    track.default = true;
-    track.label = "English";
-    track.srclang = "en"
-    track.src = "./assets/test-video.vtt";
-    // track.src = "https://beatcaps-workshop-conference.netlify.app/assets/test-video.vtt";
-    // track.addEventListener("load", () => {
-    //  this.mode = "showing";
-    //  videoNode.textTrack[0].mode = "showing";
-    // });
      videoNode = document.createElement('video');
      videoNode.setAttribute('class', 'clip-item'); // style lager
      videoNode.setAttribute('id', 'video-clip');
@@ -173,12 +162,21 @@
      videoNode.muted = false;
      videoNode.setAttribute("autoplay", 'autoplay');
      videoNode.setAttribute("src", participant.url);;
-    //  videoNode.addEventListener('loadedmetadata', () => {
-    //  });
+     videoNode.addEventListener('loadedmetadata', () => {
+       let track = document.createElement("track");
+       track.kind = "captions";
+       track.label = "English";
+       track.srclang = "en"
+       track.src = "/assets/test-video.vtt";
+       track.addEventListener("load", () => {
+        this.mode = "showing";
+        videoNode.textTrack[0].mode = "showing";
+       });
+       videoNode.prepend(track)
+     });
      
      const videoContainer = document.getElementById('video-container');
      videoContainer.prepend(videoNode)
-     videoNode.prepend(track)
    }
    navigator.attachMediaStream(videoNode, stream);
    playClipBtn.disabled = false;
@@ -191,4 +189,116 @@
    }
  };
 
- 
+/** VideoShake
+ * @param {DOMString} [element='video']
+ */
+ function videoShake(elements='video') {
+  const els = document.querySelectorAll(elements);
+
+  const keyframes = [
+      {
+          transform: "rotate(0deg)"
+      },
+      {
+          transform: "rotate(2deg)",
+          offset: 0.25
+      },
+      {
+          transform: "rotate(0deg)",
+          offset: 0.5
+      },
+      {
+          transform: "rotate(-2deg)",
+          offset: 0.75
+      },
+      {
+          transform: "rotate(0deg)",
+          offset: 1
+      },
+  ]
+  const timing = {
+      duration: 75,
+      iterations: 2
+  }
+  for (let i = 0, len = els.length; i < len; i++) {
+      els[i].animate(keyframes, timing)
+  }
+}
+
+//PIEZ
+const ERROR_NO_NAV_VIBRATE = 'No vibration API present';
+
+let _vibrateInterval;
+let vibrating = false;
+
+/**
+ * Starts vibration at passed in level
+ * @param {Number} duration milliseconds
+ */
+function _repeatVibrate(duration = 0) {
+	window.navigator.vibrate(duration);
+}
+
+/**
+ * Clear interval and stop persistent vibrating
+ * @param {Boolean} [verbose=false]
+ */
+function stopVibrate(verbose = false) {
+	if (verbose && !window.navigator.vibrate) {
+		throw new Error(ERROR_NO_NAV_VIBRATE);
+	}
+
+	if (_vibrateInterval) {
+		clearInterval(_vibrateInterval);
+	}
+
+	window.navigator.vibrate(0);
+	vibrating = false;
+}
+
+/**
+ * Getter
+ * @returns {Boolean}
+ */
+function isVibrating() {
+	return vibrating;
+}
+
+/**
+ * @param {Number} [duration=2] milliseconds
+ * @param {Number} [interval=1] milliseconds
+ * @param {Boolean} [verbose=false]
+ */
+function toggleVibrate(duration = 2, interval = 1, verbose = false) {
+	if (vibrating) {
+		stopVibrate();
+	} else {
+		startVibrate(duration, interval, verbose);
+	}
+}
+
+/**
+     * Start persistent vibration at given duration and interval
+     * @param {Number} [duration=2] milliseconds
+     * @param {Number} [interval=1] milliseconds
+     * @param {Boolean} [verbose=false]
+     */
+function startVibrate(duration = 10000, interval = 10000, verbose = false) {
+	if (verbose && !window.navigator.vibrate) {
+		throw new Error(ERROR_NO_NAV_VIBRATE);
+	}
+
+	_repeatVibrate(duration);
+	_vibrateInterval = setInterval(() => {
+		_repeatVibrate(duration);
+	}, interval);
+	vibrating = true;
+}
+
+module.exports = {
+	isVibrating,
+	startVibrate,
+	stopVibrate,
+	toggleVibrate,
+  videoShake
+};
